e79e0c19dddd29ce49cbb4dbcdea82033c8345be
Ben Baker <bensbaker35@gmail.com>
2020-05-07 21:18:09 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RecipieWebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
